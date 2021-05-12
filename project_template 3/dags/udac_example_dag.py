caed97e95b45303e8bf39b302df41179946f52eb
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-05-11 23:49:07 -0400

    Improved data quality check operator
    Check is not parameterized rather than hard coded in the operator

--- project_template 3/dags/udac_example_dag.py
+++ project_template 3/dags/udac_example_dag.py
@@ -10,56 +10,126 @@ from helpers import SqlQueries
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'owner': 'Asad Siddiqui',   
+    'depends_on_past': False,
+    'start_date': datetime(2018, 11, 1, 0, 0, 0, 0),
+    'end_date': datetime(2018, 12, 1, 0, 0, 0, 0),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          description='DAG for Data Engineering Project 5 - To load from S3 and transform data in Redshift with Airflow',          
+          schedule_interval='@daily',
+          max_active_runs=1
         )
+
+table_names=('songplays','artists','users','songs','time')
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    aws_credentials="aws_credentials",
+    redshift_conn_id='redshift',
+    table='staging_events',
+    s3_bucket='udacity-dend',
+    s3_key='log_data/{execution_date.year}/{execution_date.month}/{ds}-events.json'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    aws_credentials="aws_credentials",
+    redshift_conn_id='redshift',
+    table='staging_songs',
+    s3_bucket='udacity-dend',
+    s3_key='song_data'
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    aws_credentials='aws_credentials',
+    redshift_conn_id='redshift',
+    table='songplays',
+    sql_statement=SqlQueries.songplay_table_insert
 )
+
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='users',
+    sql_statement=SqlQueries.user_table_insert,
+    truncate='Y'
+
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songs',
+    sql_statement=SqlQueries.song_table_insert,
+    truncate='Y'
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='artists',
+    sql_statement=SqlQueries.artist_table_insert,
+    truncate='Y'
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='time',
+    sql_statement=SqlQueries.time_table_insert,
+    truncate='Y'
+
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table_names=table_names,
+    quality_checks=[
+    {'check_sql':'SELECT count(*) FROM users WHERE userid IS null', 'expected_result':0},
+    {'check_sql':'SELECT count(*) FROM songs WHERE songid IS null', 'expected_result':0},
+    {'check_sql':'SELECT count(*) FROM artists WHERE artistid IS null', 'expected_result':0},
+    {'check_sql':'SELECT count(*) FROM time WHERE start_time IS null', 'expected_result':0}
+    ]
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Task dependencies 
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_songs_to_redshift >> load_songplays_table
+stage_events_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
--- project_template 3/plugins/operators/data_quality.py
+++ project_template 3/plugins/operators/data_quality.py
@@ -18,14 +18,16 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id='',
                  table_names='',
+                 quality_checks=[],
                  *args, **kwargs):
 
         # Constructor defines instance attributes (these will be specified in the DAG file) as well as leverages the parent class's methods        
-
         
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id=redshift_conn_id
         self.table_names=table_names
+        self.quality_checks=quality_checks
+             
 
     def execute(self, context):
 
@@ -33,12 +35,40 @@ class DataQualityOperator(BaseOperator):
         This function is executed when Airflow's runner calls the operator. It has the following roles:
             - Creates neccessary hook to interact with external systems.
             - Checks whether fact and dimensions tables were populated with data.
+            
+            The way this quality check works is by using the SQL statements for checking if specific attribute of each table has 'null' value. The expected value is 0.
+            After receiving the number of records that containing 'null'value, count of error is increased by 1 if number of records containig 'null' value is not 0. 
+            The SQL statement is also pushed into a list. Lastly, if error count > 0, quality check will fail and ValueError will be raised. 
         '''
         
         self.log.info("Creating Redshift Hook by getting parameters to interact with Redshift.")
         redshift=PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info("Redshift Hook is now created.")
+        
+        error_count = 0
+        failing_tests = []
+        
+        self.log.info("Data quality checks will now begin.")        
+        for check in self.quality_checks:
+            sql = check.get('check_sql')
+            expected_result = check.get('expected_result')
+           
+            records = redshift.get_records(sql)
+            self.log.info(records[0][0])
+            
+            if expected_result != records[0][0]:
+                error_count += 1
+                failing_tests.append(sql)
 
+        if error_count > 0:
+            self.log.info('Tests failed')
+            self.log.info(failing_tests)
+            raise ValueError('Data quality check failed')
+
+        self.log.info('Data Quality checks passed!')
+            
+        # Old version of quality check
+        '''
         for table in self.table_names:
             self.log.info(f"To ensure data quality for Sparkify, check to see Table {table} is populated.")
             records = redshift.get_records(f"SELECT count(*) FROM {table}")
@@ -49,6 +79,6 @@ class DataQualityOperator(BaseOperator):
             else:
                 self.log.info(f"Data quality check passed!\nThere are {total_records} in {table}.")
            
-        
+        '''
         
         
